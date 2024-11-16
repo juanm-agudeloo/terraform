ac63c8cb2fec4bd222fc45e96534e77ae9482821
juanm-agudeloo <juanm.agudeloo@upb.edu.co>
2024-11-15 20:17:22 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install docker-compose -y
 sudo apt install git -y
-git clone https://github.com/usuario/proyecto
-cd proyecto
+git clone https://github.com/juanm-agudeloo/terraform
+cd terrform
 sudo docker-compose up -d
